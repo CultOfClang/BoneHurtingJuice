35b020a7e288de8ad3c479682bdafd28f2822bb8
Nathan Harris <43653484+pixelfac@users.noreply.github.com>
2021-02-05 09:16:19 -0500

    simplified logic

--- src/main/kotlin/org/cultofclang/bonehurtingjuice/MoveListener.kt
+++ src/main/kotlin/org/cultofclang/bonehurtingjuice/MoveListener.kt
@@ -61,10 +61,7 @@ internal object MoveListener : Listener {
 
             //bypass armor damage reduction
             player.damage(0.0001) // trigger damage sound effect
-            if (player.health > 0.25 * Bones.waterfallDamgeMultiplier)
-                player.health = player.health - (0.25 * Bones.waterfallDamgeMultiplier)
-            else
-                player.health = 0.0 //kill player
+            player.health = (player.health - (0.25 * Bones.waterfallDamgeMultiplier)).coerceAtLeast(0.0)
 
             player.world.spawnParticle(Particle.CLOUD, player.location.add(0.0, 0.75, 0.0), 1, 0.5, 0.5, 0.5, 0.3)
             player.velocity = player.velocity.apply {
